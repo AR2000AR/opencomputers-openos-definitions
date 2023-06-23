795de298b135439df7242a54da5e7b93687ec634
AR2000AR <AR2000AR@users.noreply.github.com>
2023-06-24 00:14:31 +0200

    redstone comp fix

--- library/component/redstone.lua
+++ library/component/redstone.lua
@@ -10,20 +10,20 @@ local redstone = {}
 ---@param side number
 ---@return number
 ---@overload fun():table<number>
-function getInput(side) end
+function redstone.getInput(side) end
 
 ---Gets the currently set output on the specified side, or the set value of all sides if called with no arguments
 ---@param side number
 ---@return number
 ---@overload fun():table<number>
-function getOutput(side) end
+function redstone.getOutput(side) end
 
 ---Sets the strength of the redstone signal to emit. Returns the *old* value. This can be an arbitrarily large number for mods that support this. The first variant of this method takes a side and the redstone strength to apply to just that side. `setOutput(values)` allows you to set the redstone strength of all sides (or a subset of sides) in a single call.
 ---@param side number
 ---@param value number
 ---@return number
 ---@overload fun(values:table<number>):table<number>
-function setOutput(side, value) end
+function redstone.setOutput(side, value) end
 
 ---Like `getInput`, but for bundled input, reading the value for the channel with the specified API/Colors.\
 ---As of OC 1.3: only available on a tier two redstone card.\
@@ -35,47 +35,47 @@ function setOutput(side, value) end
 ---@return number
 ---@overload fun(side:number):table
 ---@overload fun():table
-function getBundledInput(side, color) end
+function redstone.getBundledInput(side, color) end
 
 ---Like `setOutput`, but for bundled output, setting the value for the channel with the specified API/Colors. Returns the previous values set. `setBundledOutput(side, values)` sets a pack of color-indexed redstone values, [0, 15]. `colors.white` is zero. The values table doesn't have to be contiguous, and values omitted are left unchanged. `setBundledOutput(values)` allows you to set redstone levels for any side and any color in a single api call.
 ---@param side number
 ---@param color number
 ---@param value number
 ---@return number
-function setBundledOutput(side, color, value) end
+function redstone.setBundledOutput(side, color, value) end
 
 ---Get the comparator input on the specified side.
 ---@param side number
 ---@return number
-function getComparatorInput(side) end
+function redstone.getComparatorInput(side) end
 
 ---Get the wireless redstone input.
 ---@return number
-function getWirelessInput() end
+function redstone.getWirelessInput() end
 
 ---Get the wireless redstone output.
 ---@return boolean
-function getWirelessOutput() end
+function redstone.getWirelessOutput() end
 
 ---Set the wireless redstone output.
 ---@param value boolean
 ---@return boolean
-function setWirelessOutput(value) end
+function redstone.setWirelessOutput(value) end
 
 ---Get the currently set wireless redstone frequency.
 ---@return number
-function getWirelessFrequency() end
+function redstone.getWirelessFrequency() end
 
 ---Set the wireless redstone frequency to use.
 ---@param frequency number
 ---@return number
-function setWirelessFrequency(frequency) end
+function redstone.setWirelessFrequency(frequency) end
 
 ---Gets the current wake-up threshold.
 ---@return number
-function getWakeThreshold() end
+function redstone.getWakeThreshold() end
 
 ---Sets the wake-up threshold to the specified number.
 ---@param threshold number
 ---@return number
-function setWakeThreshold(threshold) end
+function redstone.setWakeThreshold(threshold) end
