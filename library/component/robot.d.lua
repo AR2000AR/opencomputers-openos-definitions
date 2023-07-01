492432f0172a72bbd44825993ef656e67d7502a8
Deculsion <unknownkiller100@gmail.com>
2023-07-01 10:43:06 +0800

    fix: fix function name and class inheritence

--- library/component/robot.d.lua
+++ library/component/robot.d.lua
@@ -1,36 +1,36 @@
 ---@meta
 
----@class ComponentRobot
+---@class ComponentRobot : Component
 local robot = {}
 
 --#region Base methods
 ---Get the durability of the currently equipped tool.
 ---@return number
-function durability()
+function robot.durability()
 end
 
 ---Move in the specified direction. 
 ---@param direction number
 ---@return boolean
-function move(direction)
+function robot.move(direction)
 end
 
 ---Rotate in the specified direction.
 ---@param clockwise boolean
 ---@return boolean
-function turn(clockwise)
+function robot.turn(clockwise)
 end
 
 ---Get the name of the robot.
 ---@return string
-function name()
+function robot.name()
 end
 
 ---Lets the robot use the currently equipped item in the tool slot against the block or space directly in front of the robot. 
 ---Returns true if successful, false if the operation fails with a description of why it failed.
 ---@param side number
 ---@return boolean, nil|string
-function swing(side)
+function robot.swing(side)
 end
 
 ---Attempts to use the item currently equipped in the tool slot in the same way as if the player would make a right-click.
@@ -43,7 +43,7 @@ end
 ---@param sneaky? boolean
 ---@param duration? number
 ---@return boolean, nil|string
-function use(side, sneaky, duration)
+function robot.use(side, sneaky, duration)
 end
 
 ---Tries to place the block in the currently selected inventory slot on the specified side of the robot 
@@ -53,17 +53,17 @@ end
 ---@param side number
 ---@param sneaky boolean
 ---@return boolean, nil|string
-function place(side, sneaky)
+function robot.place(side, sneaky)
 end
 
 ---Get the current color of the activity(robot) or flap(drone) light as an integer encoded RGB value (0xRRGGBB)
 ---@return number
-function getLightColor()    
+function robot.getLightColor()    
 end
 
 ---Set the color of the activity(robot) or flap(drone) light to the specified integer encoded RGB value (0xRRGGBB)
 ---@return number
-function setLightColor()
+function robot.setLightColor()
 end
 
 --#endregion
@@ -72,73 +72,73 @@ end
 
 ---Returns the size of the device's internal inventory.
 ---@return number
-function inventorySize()
+function robot.inventorySize()
 end
 
 ---Get the currently selected slot; set the selected slot if specified.
 ---@param slot? number
 ---@return number
-function selected(slot)
+function robot.selected(slot)
 end
 
 ---Get the number of items in the specified slot, otherwise in the selected slot.
 ---@params slot? number
 ---@return number
-function count(slot)
+function robot.count(slot)
 end
 
 ---Get the remaining space in the specified slot, otherwise in the selected slot.
 ---@param slot? number
 ---@return number
-function space(slot)
+function robot.space(slot)
 end
 
 ---Compare the contents of the selected slot (in the robot inventory) to the contents of the specified slot (also in the robot inventory).
 ---@param otherSlot number
 ---@return boolean
-function compareTo(otherSlot)
+function robot.compareTo(otherSlot)
 end
 
 ---Move the specified amount of items from the selected slot into the specified slot. If no amount is specified, the entire stack is moved to the target slot.
 ---@param toSlot number
 ---@param amount? number
 ---@return boolean
-function transferTo(toSlot, amount)
+function robot.transferTo(toSlot, amount)
 end
 
 ---Returns the number of tanks installed in the robot.
 ---@return number
-function tankCount()
+function robot.tankCount()
 end
 
 ---Selects the specified tank (if robot contains more than one Tank upgrade). Any tank operations will use this tank.
 ---@param tank number
-function selectTank(tank)
+function robot.selectTank(tank)
 end
 
 ---Returns the fluid level in the specified tank. If no tank specified, returns the fluid level in the selected tank (using selectTank()).
 ---@param tank? number
 ---@return number
-function tankLevel(tank)
+function robot.tankLevel(tank)
 end
 
 ---Returns the remaining fluid capacity (empty space) in the specified tank. If not tank specified, returns the remaining capacity in the selected tank.
 ---@param tank? number
 ---@return number
-function tankSpace(tank)
+function robot.tankSpace(tank)
 end
 
 ---Tests whether the fluid in the selected tank (in the robot inventory) is the same as in the specified tank (requires Tank upgrade).
 ---@param tank number
 ---@return boolean
-function compareFluidTo(tank)
+function robot.compareFluidTo(tank)
 end
 
 ---Transfer the specified amount of fluid in the selected tank into the specified tank. If no volume is specified, the robot will attempt to transfer 1000mB.
 ---@param tank number
 ---@param count? number
 ---@return boolean
-function transferFluidTo(tank, count)
+function robot.transferFluidTo(tank, count)
 end
 
 --#endregion
@@ -152,13 +152,13 @@ end
 ---Drones return true even if the block is passable.
 ---@param side number
 ---@return boolean
-function detect(side)
+function robot.detect(side)
 end
 
 ---Compares fluid in the selected tank to fluid in the world or in an external tank on the specified side of the robot.
 ---@param side number
 ---@return boolean
-function compareFluid(side)
+function robot.compareFluid(side)
 end
 
 ---Extracts the specified amount of fluid from the world or a tank on the specified side of the robot.
@@ -167,7 +167,7 @@ end
 ---@param side number
 ---@param count? number
 ---@return boolean
-function drain(side, count)
+function robot.drain(side, count)
 end
 
 ---Fills the specified amount of fluid from the selected tank into the world or a tank in front of the robot. 
@@ -176,14 +176,14 @@ end
 ---@param side number
 ---@param count? number
 ---@return boolean
-function fill(side, count)
+function robot.fill(side, count)
 end
 
 ---Compares the block on the specified side of the robot with the item in the currently selected slot and returns whether they are the same or not.
 ---Blocks are considered identical if the type and metadata matches.
 ---@param side number
 ---@param fuzzy? boolean 
-function compare(side, fuzzy)
+function robot.compare(side, fuzzy)
 end
 
 ---Drops the specified number of items from the currently selected slot.
@@ -193,7 +193,7 @@ end
 ---@param side number
 ---@param count? number
 ---@return boolean
-function drop(side, count)
+function robot.drop(side, count)
 end
 
 ---Tries to pick up the specified number of items and place it in the selected slot. 
@@ -202,7 +202,7 @@ end
 ---@param side number
 ---@param count? number
 ---@return boolean
-function suck(side, count)
+function robot.suck(side, count)
 end
 
 --#endregion
